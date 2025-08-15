71dfbe34dac44b2c27d956ec8538611fbcacc073
P/ <ve2caz@users.noreply.github.com>
2025-08-15 15:48:55 -0400

    Update .brewignore and .gitignore for Atlassian CLI; enhance brew-dump-clean.sh to remove Atlassian entries

--- scripts/brew-dump-clean.sh
+++ scripts/brew-dump-clean.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # Generate Brewfile without VS Code extensions
 brew bundle dump --force --describe --no-vscode
-echo "Brewfile updated excluding VS Code extensions."
+# Remove Atlassian CLI entries
+# The .brewignore functionality isn't working properly
+sed -i '' '/^tap "atlassian\/acli"/d' Brewfile
+sed -i '' '/^# Software to interact with Atlassian Cloud from the terminal/d' Brewfile
+sed -i '' '/^brew "atlassian\/acli\/acli"/d' Brewfile
+echo "Brewfile updated, Atlassian CLI entries removed."
